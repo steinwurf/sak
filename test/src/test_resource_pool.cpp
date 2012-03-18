1b774a945fd01bbf895d96efe3ff7d281c54c1ac
Morten V. Pedersen <morten@mortenvp.dk>
2012-03-18 23:49:20 +0100

    Updated license to new BSD

--- test/src/test_resource_pool.cpp
+++ test/src/test_resource_pool.cpp
@@ -1,29 +1,27 @@
-// Copyright (c) 2011, Steinwurf ApS
+// Copyright (c) 2012, Steinwurf ApS
 // All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met: 
 //
-// 1. Redistributions of source code must retain the above copyright notice, this
-//    list of conditions and the following disclaimer. 
-// 2. Redistributions in binary form must reproduce the above copyright notice,
-//    this list of conditions and the following disclaimer in the documentation
-//    and/or other materials provided with the distribution. 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Steinwurf ApS nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 // (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 // LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 // ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// The views and conclusions contained in the software and documentation are those
-// of the authors and should not be interpreted as representing official policies, 
-// either expressed or implied, of the FreeBSD Project.
 
 #include <stdint.h>
 #include <gtest/gtest.h>
@@ -40,7 +38,7 @@ struct dummy_object
 	{
 	    --m_count;
 	}
-    
+
 
     static int32_t m_count;
 };
@@ -74,17 +72,17 @@ TEST(TestResourcePool, Construct)
 
 	ASSERT_TRUE(pool.size() == 1);
 	ASSERT_TRUE(pool.free() == 1);
-	
+
 	dummy_ptr d2 = pool.allocate();
 
 	ASSERT_TRUE(pool.size() == 1);
 	ASSERT_TRUE(pool.free() == 0);
-	
+
 	dummy_ptr d3 = pool.allocate();
 
 	ASSERT_TRUE(pool.size() == 2);
 	ASSERT_TRUE(pool.free() == 0);
-	
+
 	ASSERT_TRUE(dummy_object::m_count == 2);
 
 	{
@@ -96,11 +94,11 @@ TEST(TestResourcePool, Construct)
 
 	ASSERT_TRUE(pool.size() == 3);
 	ASSERT_TRUE(pool.free() == 1);
-	
+
     }
-    
+
     ASSERT_TRUE(dummy_object::m_count == 0);
-    
+
 }
 
 
@@ -111,20 +109,20 @@ TEST(TestResourcePool, PoolDieBeforeObject)
 	dummy_ptr d1;
 	dummy_ptr d2;
 	dummy_ptr d3;
-	
+
 	{
 	    sak::resource_pool<dummy_object> pool( boost::bind(make_dummy) );
-	    
+
 	    d1 = pool.allocate();
-	    d2 = pool.allocate();	
+	    d2 = pool.allocate();
 	    d3 = pool.allocate();
-	    
+
 	    ASSERT_TRUE(pool.size() == 3);
 	}
 
     }
     ASSERT_TRUE(dummy_object::m_count == 0);
-    
+
 }
 
 

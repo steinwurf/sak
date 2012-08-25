ccb8bd3ce52bbc85948deececc53c4753bba9f8d
Jeppe Pihl <jpihl08@gmail.com>
2012-08-25 16:19:23 +0200

    updated waf, added tests, and comments,  and fixed copyright statments.

--- src/sak/endian_buffer.cpp
+++ src/sak/endian_buffer.cpp
@@ -1,8 +1,33 @@
-#include "endian_buffer.h"
+// Copyright (c) 2011-2012 Steinwurf ApS
+// All Rights Reserved
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Steinwurf ApS nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL Steinwurf ApS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "endian_buffer.hpp"
 
 #include "convert_endian.h"
 
-#include <assert.h>
+#include <cassert>
 
 namespace sak
 {
@@ -12,7 +37,8 @@ namespace sak
           m_size(size)
     {
         assert(m_buffer != 0);
-        assert(m_size != 0);
+        assert(m_position == 0);
+        assert(m_size);
     }
 
     void endian_buffer::write_u8(uint8_t v)
@@ -63,12 +89,12 @@ namespace sak
         return big_endian::get<uint64_t>(m_buffer + m_position);
     }
 
-    uint32_t endian_buffer::size()
+    uint32_t endian_buffer::size() const
     {
         return m_size;
     }
 
-    uint32_t endian_buffer::position()
+    uint32_t endian_buffer::position() const
     {
         return m_position;
     }

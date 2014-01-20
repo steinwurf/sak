5ecca9ac45f5d74b0457db1f31b3e18ba6a7f321
Morten V. Pedersen <morten@mortenvp.dk>
2014-01-20 09:49:04 +0100

    Re-enable file_input_stream test

--- test/src/test_file_input_stream.cpp
+++ test/src/test_file_input_stream.cpp
@@ -34,63 +34,63 @@
 
 
 /// Tests reading a file, the file is crated a priori
-// TEST(TestFileInputStream, ReadRandomFile)
-// {
-//     srand(static_cast<uint32_t>(time(0)));
-//
-//     {
-//         uint32_t file_size = 1000000;
-//         std::string file_name("test.txt");
-//
-//         std::vector<char> output_buffer(file_size, '\0');
-//
-//         for (uint32_t i = 0; i < file_size; ++i)
-//         {
-//             output_buffer[i] = (rand() % 255);
-//         }
-//
-//         std::ofstream output_file(file_name.c_str(),
-//                                   std::ios::out | std::ios::binary);
-//         output_file.write(&output_buffer[0], file_size);
-//         output_file.close();
-//
-//
-//         // Now test we can read it back
-//         sak::file_input_stream fs;
-//
-//         fs.open(file_name);
-//
-//         ASSERT_EQ(file_size, fs.bytes_available());
-//
-//         uint32_t read_size = 512;
-//
-//         std::vector<char> input_buffer;
-//
-//         while (fs.bytes_available() > 0)
-//         {
-//             uint32_t read = std::min(read_size, fs.bytes_available());
-//
-//             ASSERT_TRUE(read <= read_size);
-//
-//             std::vector<char> temp(read, '\0');
-//             fs.read(reinterpret_cast<uint8_t*>(&temp[0]), read);
-//
-//             input_buffer.insert(input_buffer.end(),
-//                                 temp.begin(),
-//                                 temp.end());
-//
-//         }
-//
-//         bool result = std::equal( input_buffer.begin(),
-//                                   input_buffer.end(),
-//                                   output_buffer.begin() );
-//
-//
-//         ASSERT_TRUE(result);
-//
-//     }
-// }
-//
+TEST(TestFileInputStream, ReadRandomFile)
+{
+    srand(static_cast<uint32_t>(time(0)));
+
+    {
+        uint32_t file_size = 1000000;
+        std::string file_name("test.txt");
+
+        std::vector<char> output_buffer(file_size, '\0');
+
+        for (uint32_t i = 0; i < file_size; ++i)
+        {
+            output_buffer[i] = (rand() % 255);
+        }
+
+        std::ofstream output_file(file_name.c_str(),
+                                  std::ios::out | std::ios::binary);
+        output_file.write(&output_buffer[0], file_size);
+        output_file.close();
+
+
+        // Now test we can read it back
+        sak::file_input_stream fs;
+
+        fs.open(file_name);
+
+        ASSERT_EQ(file_size, fs.bytes_available());
+
+        uint32_t read_size = 512;
+
+        std::vector<char> input_buffer;
+
+        while (fs.bytes_available() > 0)
+        {
+            uint32_t read = std::min(read_size, fs.bytes_available());
+
+            ASSERT_TRUE(read <= read_size);
+
+            std::vector<char> temp(read, '\0');
+            fs.read(reinterpret_cast<uint8_t*>(&temp[0]), read);
+
+            input_buffer.insert(input_buffer.end(),
+                                temp.begin(),
+                                temp.end());
+
+        }
+
+        bool result = std::equal( input_buffer.begin(),
+                                  input_buffer.end(),
+                                  output_buffer.begin() );
+
+
+        ASSERT_TRUE(result);
+
+    }
+}
+
 // /// Tests error handling with exception
 // TEST(TestFileInputStream, ExceptionThrow)
 // {
